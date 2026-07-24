9901afb83f17fb88f7d6861d3e4fe571c7195f48
GAYATRI SANTOSH DABARE <gayatridabare@gmail.com>
2026-07-24 14:12:10 +0000

    Sync LeetCode submission - Length of Last Word (java)

---
+++ problems/length_of_last_word/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int count=0;
+      s=s.trim();
+        for(int i = s.length()-1;i>=0;i--){
+               if(s.charAt(i)!=' '){
+            count++;
+
+               }else{
+
+            return count;
+               }
+            
+              
+                
+            
+                
+        }
+
+    
+    return count;
+}
+}
