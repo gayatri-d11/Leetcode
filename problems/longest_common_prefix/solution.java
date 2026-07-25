e165cf7d71aefe1b16f2ac0d5d82f2874369d6ba
GAYATRI SANTOSH DABARE <gayatridabare@gmail.com>
2026-07-25 17:02:35 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- problems/longest_common_prefix/solution.java
+++ problems/longest_common_prefix/solution.java
@@ -1,22 +1,15 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int n = strs.length;
-    if(strs==null || strs.length==0){
-        return "";
-    }
+        
+        for(int i =0;i<strs[0].length();i++){
+            char ch = strs[0].charAt(i);
 
-    for(int i =0;i<strs[0].length();i++){
-        int c = strs[0].charAt(i);
-
-        for(int j=1;j<strs.length;j++){
-
-            if( i>=strs[j].length()|| strs[j].charAt(i)!=c ){
-                return strs[0].substring(0,i);
+            for(int j =1;j<strs.length;j++ ){
+                if(i>=strs[j].length()|| strs[j].charAt(i)!=ch){
+                     return strs[0].substring(0,i);
+                }
             }
         }
-    }
-return strs[0];
-
-
+        return strs[0];
     }
 }
