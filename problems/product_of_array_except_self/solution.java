7b21a65aadc8971021fb637c93243c53c9f84e58
GAYATRI SANTOSH DABARE <gayatridabare@gmail.com>
2026-08-20 17:20:09 +0000

    Sync LeetCode submission - Product of Array Except Self (java)

--- problems/product_of_array_except_self/solution.java
+++ problems/product_of_array_except_self/solution.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int ans[] = new int[n];
-        Arrays.fill(ans,1);
-
-        int curr =1;
+        int n=nums.length;
+        int[] left =new int[n];
+        int leftproduct =1;
         for(int i =0;i<n;i++){
-            ans[i] *=curr;
-            curr *=nums[i];
+              leftproduct*=nums[i];
+              left[i]= leftproduct;
 
         }
-        curr=1;
-        for(int i=n-1;i>=0;i--){
-            ans[i] *=curr;
-            curr *=nums[i];
-        }
-        return ans;
+        int[] right =new int[n];
+        int rightproduct =1;
+        for(int j =n-1;j>=0;j--){
+              rightproduct*=nums[j];
+            right[j]= rightproduct;
 
+        }
+         int[] result =new int[n];
+        for(int i =0;i<n;i++){
+            if(i==0){
+                   result[i]=right[i+1];
+            }
+            else if(i==n-1){
+                   result[i]=left[i-1];
+            }else{
+              result[i]=left[i-1]*right[i+1];
+            }
+        }
+        return result;
     }
 }
