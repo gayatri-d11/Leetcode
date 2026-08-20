5fb850f3c2067e2b4ff59e6ad16a9ba5a62f5c13
GAYATRI SANTOSH DABARE <gayatridabare@gmail.com>
2026-08-20 19:02:43 +0000

    Sync LeetCode submission - List the Products Ordered in a Period (mysql)

---
+++ problems/list_the_products_ordered_in_a_period/solution.sql
@@ -0,0 +1,9 @@
+SELECT
+    P.product_name,
+    SUM(O.unit) AS unit
+FROM Products P
+JOIN Orders O
+ON P.product_id = O.product_id
+WHERE O.order_date LIKE '2020-02%'
+GROUP BY P.product_id, P.product_name
+HAVING SUM(O.unit) >= 100;
